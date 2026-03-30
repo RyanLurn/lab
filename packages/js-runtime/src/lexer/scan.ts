d53598188c716317edc7096ca19e7393ee427412
Ryan Luong <lumintriet@gmail.com>
2026-03-30 10:22:08 +0700

    feat(js-runtime/lexer): define scan function.

---
+++ packages/js-runtime/src/lexer/scan.ts
@@ -0,0 +1,9 @@
+import { stripBOM } from "@/utils/strip-bom";
+
+export function scan(sourceText: string) {
+  const characters = stripBOM(sourceText).split("");
+
+  for (const character of characters) {
+    console.log(character);
+  }
+}
