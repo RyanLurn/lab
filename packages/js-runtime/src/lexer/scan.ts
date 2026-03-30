d0bc08aca78f7d96bb9e31393b6f719f9225d8a2
Ryan Luong <lumintriet@gmail.com>
2026-03-30 18:07:41 +0700

    draft work.

--- packages/js-runtime/src/lexer/scan.ts
+++ packages/js-runtime/src/lexer/scan.ts
@@ -1,9 +1,3 @@
-import { stripBOM } from "@/utils/strip-bom";
-
 export function scan(sourceText: string) {
-  const characters = stripBOM(sourceText).split("");
-
-  for (const character of characters) {
-    console.write(character);
-  }
+  return sourceText;
 }
