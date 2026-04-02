d7560c91f33b6c8b82fee5af3aa19129e798899d
Ryan Luong <lumintriet@gmail.com>
2026-04-02 19:00:53 +0700

    feat(axon/server/chat): define websocket handlers.

--- axon/server/src/index.ts
+++ axon/server/src/index.ts
@@ -1,9 +1,26 @@
+import { upgradeWebSocket } from "hono/bun";
 import { Hono } from "hono";
 
 import { db } from "@/mock-db";
 
 export const app = new Hono()
-  .get("/", (c) => c.text("Hello Bun!"))
+  .get("/", (context) => context.text("Hello Bun!"))
   .get("/chat-messages", (context) => {
     return context.json({ chatMessages: db.chatMessages });
-  });
+  })
+  .get(
+    "/ws",
+    upgradeWebSocket(() => {
+      return {
+        onClose: (evt, ws) => {
+          console.log("Connection closed.", { evt, ws });
+        },
+        onMessage(evt, ws) {
+          console.log("Message received:", { evt, ws });
+        },
+        onOpen(evt, ws) {
+          console.log("Connection opened.", { evt, ws });
+        },
+      };
+    })
+  );
--- axon/server/src/serve.ts
+++ axon/server/src/serve.ts
@@ -1,8 +1,10 @@
 import { AXON_SERVER_PORT } from "@lab/core/constants/ports";
+import { websocket } from "hono/bun";
 
 import { app } from "@/index";
 
 export default {
   port: AXON_SERVER_PORT,
   fetch: app.fetch,
+  websocket,
 };
