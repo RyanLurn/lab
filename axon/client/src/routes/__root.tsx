9c8cbfe537d03cc95ac5257a695a1757280fc288
Ryan Luong <lumintriet@gmail.com>
2026-04-02 08:59:52 +0700

    feat(axon/client/ui): add theme to UI.

--- axon/client/src/routes/__root.tsx
+++ axon/client/src/routes/__root.tsx
@@ -1,5 +1,16 @@
+import { ModeToggle } from "@lab/ui/features/theme/components/mode-toggle";
+import { ThemeProvider } from "@lab/ui/features/theme/components/provider";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-
-const RootLayout = () => <Outlet />;
+import { Toaster } from "@lab/ui/components/toaster";
 
 export const Route = createRootRoute({ component: RootLayout });
+
+function RootLayout() {
+  return (
+    <ThemeProvider>
+      <Toaster position="top-center" closeButton richColors />
+      <Outlet />
+      <ModeToggle className="fixed top-3 right-3 z-50" />
+    </ThemeProvider>
+  );
+}
--- axon/client/src/routes/index.tsx
+++ axon/client/src/routes/index.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@lab/ui/components/button";
+import { toast } from "@lab/ui/components/toaster";
 
-export const Route = createFileRoute("/")({
-  component: Index,
-});
+export const Route = createFileRoute("/")({ component: IndexPage });
 
-function Index() {
-  return <h1>Welcome Home!</h1>;
+function IndexPage() {
+  return (
+    <div className="flex h-dvh flex-col items-center justify-center gap-y-2">
+      <h1 className="text-2xl font-bold">Home</h1>
+      <Button onClick={() => toast.success("Yippee!")}>Click me!</Button>
+    </div>
+  );
 }
