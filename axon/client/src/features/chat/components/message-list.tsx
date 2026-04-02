20a9058e8224865376efb9e68a3e0a8b25113cb0
Ryan Luong <lumintriet@gmail.com>
2026-04-02 10:03:12 +0700

    feat(axon/client/chat/ui): define MessageList component.

---
+++ axon/client/src/features/chat/components/message-list.tsx
@@ -0,0 +1,13 @@
+import type { ChatMessage } from "@/features/chat/types";
+
+import { MessageBubble } from "@/features/chat/components/message-bubble";
+
+export function MessageList(chatMessages: ChatMessage[]) {
+  return (
+    <div className="flex w-full flex-1 flex-col items-start gap-y-4">
+      {chatMessages.map((chatMessage) => (
+        <MessageBubble key={chatMessage.id} {...chatMessage} />
+      ))}
+    </div>
+  );
+}
