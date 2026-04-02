fdc33333fec298950970a817a7573b9e7b8bb11a
Ryan Luong <lumintriet@gmail.com>
2026-04-02 20:07:58 +0700

    feat(core/error): make cause a prop in the base interface.

--- packages/core/src/error/types.ts
+++ packages/core/src/error/types.ts
@@ -1,23 +1,21 @@
+/* eslint-disable perfectionist/sort-interfaces */
 import type { ErrorObject } from "serialize-error";
 
 import type { SerializedUnknown } from "@/utils/serialize-unknown";
 
+export interface CustomError {
+  code: string;
+  message: string;
+  cause?: SerializedUnknown | ErrorObject;
+  context?: Record<string, unknown>;
+}
+
 export interface UnexpectedError extends CustomError {
-  context: {
-    cause: ErrorObject;
-  };
   code: "UNEXPECTED_ERROR";
+  cause: ErrorObject;
 }
 
 export interface UnknownError extends CustomError {
-  context: {
-    cause: SerializedUnknown;
-  };
   code: "UNKNOWN_ERROR";
-}
-
-export interface CustomError {
-  context?: Record<string, unknown>;
-  message: string;
-  code: string;
+  cause: SerializedUnknown;
 }
