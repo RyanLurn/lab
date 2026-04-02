ad1cf8930ff9a36ccefff10ca1f9bf0f58d7f6d7
Ryan Luong <lumintriet@gmail.com>
2026-04-02 18:10:36 +0700

    feat(axon/server/routes): GET chatMessages endpoint.

--- axon/server/src/index.ts
+++ axon/server/src/index.ts
@@ -1,7 +1,13 @@
 import { AXON_SERVER_PORT } from "@lab/core/constants/ports";
 import { Hono } from "hono";
 
-const app = new Hono().get("/", (c) => c.text("Hello Bun!"));
+import { db } from "@/mock-db";
+
+const app = new Hono()
+  .get("/", (c) => c.text("Hello Bun!"))
+  .get("/chat-messages", (context) => {
+    return context.json({ chatMessages: db.chatMessages });
+  });
 
 export default {
   port: AXON_SERVER_PORT,
